32d4dd3dcef39956b8d976ca0d6230246ea260ac
Ravil Latypov <ravillatypov12@gmail.com>
2021-02-15 18:54:30 +0300

    add call records

---
+++ src/components/calls/CallRecordingDialog.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-tooltip
+    left
+    v-model="show"
+  >
+    <template v-slot:activator>
+      <v-btn
+        icon
+      >
+        <v-icon @click="show = !show">
+          mdi-play-circle-outline
+        </v-icon>
+      </v-btn>
+    </template>
+    <audio controls :src="callRecordUrl"></audio>
+  </v-tooltip>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  props: ['callId'],
+  data () {
+    return {
+      show: false
+    }
+  },
+  computed: {
+    callRecordUrl () {
+      return `${this.apiUrl}/api/v1/record/?call_id=${this.callId}`
+    },
+    ...mapGetters(['apiUrl'])
+  }
+}
+</script>
--- src/components/calls/CallsList.vue
+++ src/components/calls/CallsList.vue
@@ -13,10 +13,10 @@
       ></v-text-field>
     </v-card-title>
     <v-card-subtitle>
-      <calls-filters />
+      <calls-filters/>
     </v-card-subtitle>
     <v-data-table
-      :headers="headers"
+      :headers="availableHeaders"
       :items="calls"
       :search="search"
       multi-sort
@@ -24,18 +24,23 @@
       class="elevation-1"
     >
       <template v-slot:item.started_at="{ item }">
-        {{ item.started_at | moment("DD.MM.YYYY, HH:mm:ss") }}
+        {{ item.started_at | moment('DD.MM.YYYY, HH:mm:ss') }}
       </template>
 
       <template v-slot:item.state="{ item }">
         <span v-if="item.state == 'Пропущен'" class="red--text">{{
-          item.state
-        }}</span>
+            item.state
+          }}</span>
         <span v-else>{{ item.state }}</span>
       </template>
 
       <template v-slot:item.is_record="{ item }">
-        <v-icon dense v-if="item.is_record">mdi-play-circle-outline</v-icon>
+        <div v-if="item.is_record">
+          <CallRecordingDialog :id="item.id"/>
+          <v-btn icon color="indigo" link :href="callRecordUrl(item.id)" target="_blank">
+            <v-icon dense>mdi-download-circle-outline</v-icon>
+          </v-btn>
+        </div>
       </template>
     </v-data-table>
   </v-card>
@@ -44,54 +49,138 @@
 <script>
 import { mapActions, mapGetters } from 'vuex'
 import CallsFilters from './CallFiltersForm'
+import CallRecordingDialog from '@/components/calls/CallRecordingDialog'
 
 export default {
   data () {
     return {
+      showPlayerCallId: null,
       selectedState: 'NEED_RECALL',
       selectedCallType: null,
       statesSelect: [
-        { state: 'NEED_RECALL', title: 'Необходимо перезвонить' },
-        { state: 'NEW', title: 'Новый' },
-        { state: 'CONNECTED', title: 'Начат' },
-        { state: 'END', title: 'Успешный' },
-        { state: 'MISSED', title: 'Пропущен' },
-        { state: 'NOT_CONNECTED', title: 'Недозвон' }
+        {
+          state: 'NEED_RECALL',
+          title: 'Необходимо перезвонить'
+        },
+        {
+          state: 'NEW',
+          title: 'Новый'
+        },
+        {
+          state: 'CONNECTED',
+          title: 'Начат'
+        },
+        {
+          state: 'END',
+          title: 'Успешный'
+        },
+        {
+          state: 'MISSED',
+          title: 'Пропущен'
+        },
+        {
+          state: 'NOT_CONNECTED',
+          title: 'Недозвон'
+        }
       ],
       callTypeSelect: [
-        { type: 'INCOMING', title: 'Входящий' },
-        { type: 'OUTBOUND', title: 'Исходящий' },
-        { type: 'INTERNAL', title: 'Внутренний' }
+        {
+          type: 'INCOMING',
+          title: 'Входящий'
+        },
+        {
+          type: 'OUTBOUND',
+          title: 'Исходящий'
+        },
+        {
+          type: 'INTERNAL',
+          title: 'Внутренний'
+        }
       ],
       showFilter: false,
       ordering: '-created_at',
       search: '',
       callList: [],
       headers: [
-        { text: 'Дата', align: 'start', value: 'started_at', groupable: false },
-        { text: 'Тип', value: 'call_type', groupable: false },
-        { text: 'Кто', value: 'a_number', groupable: false },
-        { text: 'Внешний номер', value: 'ext_number', groupable: false },
-        { text: 'Кому', value: 'b_number', groupable: false },
-        { text: 'Ожидание', value: 'waiting_time', groupable: false },
-        { text: 'Длительность', value: 'duration', groupable: false },
-        { text: 'Статус', value: 'state', groupable: false },
-        { text: 'Запись', value: 'is_record', groupable: false }
-        // { text: "Метки", value: "tags", groupable: false },
+        {
+          text: 'Дата',
+          align: 'start',
+          value: 'started_at',
+          groupable: false,
+          permission: null
+        },
+        {
+          text: 'Тип',
+          value: 'call_type',
+          groupable: false,
+          permission: null
+        },
+        {
+          text: 'Кто',
+          value: 'a_number',
+          groupable: false,
+          permission: null
+        },
+        {
+          text: 'Внешний номер',
+          value: 'ext_number',
+          groupable: false,
+          permission: null
+        },
+        {
+          text: 'Кому',
+          value: 'b_number',
+          groupable: false,
+          permission: null
+        },
+        {
+          text: 'Ожидание',
+          value: 'waiting_time',
+          groupable: false,
+          permission: null
+        },
+        {
+          text: 'Длительность',
+          value: 'duration',
+          groupable: false,
+          permission: null
+        },
+        {
+          text: 'Статус',
+          value: 'state',
+          groupable: false,
+          permission: null
+        },
+        {
+          text: 'Запись',
+          value: 'is_record',
+          groupable: false,
+          permission: 3
+        }
+        // { text: "Метки", value: "tags", permission: null },
       ]
     }
   },
   computed: {
-    ...mapGetters(['calls'])
+    availableHeaders () {
+      return this.headers.filter((i) => {
+        return (i.permission === null || this.permissions.includes(i.permission))
+      })
+    },
+    ...mapGetters(['calls', 'permissions', 'apiUrl'])
   },
   mounted () {
     this.getCalls()
   },
   methods: {
+    callRecordUrl (callId) {
+      return `${this.apiUrl}/api/v1/record/?call_id=${callId}`
+    },
     ...mapActions(['getCalls'])
   },
   components: {
-    CallsFilters
+    CallsFilters,
+    CallRecordingDialog
   }
 }
 </script>
